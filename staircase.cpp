96911ed6a9afafca0191d56d4219cf453666e99a
Akki <132276311+Akhilesh2220@users.noreply.github.com>
2023-10-23 23:02:45 +0530

    Create staircase.cpp

---
+++ staircase.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void staircase(int n){
+	for(int i=1;i<=n;i++){
+		for(int j=i;j<=n-1;j++){
+			cout<<" ";
+		}
+		for(int k=1;k<=i;k++){
+			cout<<"#";
+		}
+		cout<<endl;
+	}
+}
+int main(){
+	int n; cin>>n;
+	staircase(n);	
+}
